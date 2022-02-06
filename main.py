f81a3e1cdb62a30b402bb90404baf8574f8446af
Piplup Owo <82926705+piplupOwo@users.noreply.github.com>
2022-02-06 02:39:32 +0000

    keep alive

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
 def home():
-  return "home page"
+    return "Hello. I am alive!"
 
-  
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
