9243bd6e5cfaa2e42a41aed5925df8caba5b9371
Piplup Owo <82926705+piplupOwo@users.noreply.github.com>
2022-02-07 06:12:56 +0000

    1st api?

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 <a href="https://piplupOwo.adamho2.repl.co"> website (i hope its up :/) </a>
 
 <img src="https://cdn.discordapp.com/attachments/818102115058450474/887315665273368596/pipbw.gif" style="opacity: 0.1" /><a href="https://github.com/piplupOwo/flask_piplupOwo/blob/main/main.py"> main python file </a> 
+<p>what it can do for now:</p>
+<ul>
+<li>random number api: <br><a href='https://flaskpiplupowo.adamho2.repl.co/random/100/'>get a random number from 0 to 100 (inclusive) in a json format</a></li>
+</ul>
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from threading import Thread
 import time
+import random
+
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+  htmlstring = "<p>Hello world!</p>"
+  htmlstring += "<a href='/test'>test page</a><br>"
+  htmlstring += "<a href='/random/'>generate random number page</a>"
+  return htmlstring
+
+@app.route('/test/')
+def test():
+  return "hello test page!<br><a href='/'>back to home page</a>"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+  t = Thread(target=run)
+  t.start()
 
 def background_task():
   count = 0
@@ -22,7 +31,34 @@ def background_task():
     count += 1
     time.sleep(5)
 
-keep_alive()
+def run_bg_task():
+  bgthread = Thread(target=background_task)
+  bgthread.start()
 
-bgthread = Thread(target=background_task)
-bgthread.start()
+
+@app.route('/random/<number>/')
+def randompage(number):
+  print("random page visited")
+  random_number = random.randint(0, int(number))
+  returnjson = {"number" : random_number}
+  htmlstring = returnjson
+
+  return htmlstring
+
+@app.route('/random/')
+def randomstartpage():
+  htmlstring = f"<p>random start page</p>"
+  htmlstring += "<a href='/'>back to home page</a>"
+  htmlstring += "<h1>generate a random number from 0 to :</h1><ul>"
+
+  for i in range(0, 101, 10):
+    htmlstring += f"<li><a href='{i}'>{i}</a></li>"
+
+  htmlstring += "</ul>"
+
+  return htmlstring
+
+
+
+keep_alive()
+run_bg_task()
